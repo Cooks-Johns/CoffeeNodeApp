ce152643bce5fe74e3ef19b5aae5f81ece9444ee
Cooks-Johns <cooks.johns@gmail.com>
2021-03-03 21:34:43 -0600

    first push

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+import logo from './logo1.jpg';
 import './App.css';
 
 function App() {
@@ -6,19 +6,48 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+
+        <h1>Coffee Application!</h1>
+
+        <form>
+            <label for="roast-selection"></label>
+            <select id="roast-selection">
+                <option>light</option>
+                <option>medium</option>
+                <option>dark</option>
+            </select>
+            <input id="submit" type="submit" />
+        </form>
+
+        <table>
+            <thead>
+                <tr>
+                    <th>ID</th>
+                    <th>NAME</th>
+                    <th>ROAST</th>
+                </tr>
+            </thead>
+            <tbody id="coffees"></tbody>
+        </table>
+
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Please <code>check out</code> gitHub.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/Cooks-Johns/coffee-app"
           target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          rel="noopener noreferrer">
+          click me
+        </a> 
+
+        <script src="main.js"></script>
       </header>
+
+      
     </div>
+
+    
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -8,9 +8,15 @@ ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
+
+
+  
   document.getElementById('root')
 );
 
+function Welcome(props) {
+  return <h1>Hello, {props.name}</h1>;
+}
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
